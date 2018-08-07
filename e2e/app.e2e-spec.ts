2e4f3b5188bb4044456e2a2a688378faf46c1f90
lakshana36 <saravananlakshana@gmail.com>
2018-08-08 00:23:24 +0530

    sample testing

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,5 +1,17 @@
 import { AppPage } from './app.po';
 
+// describe('letslearn App', () => {
+//   let page: AppPage;
+
+//   beforeEach(() => {
+//     page = new AppPage();
+//   });
+
+//   it('should display welcome message', () => {
+//     page.navigateTo();
+//     expect(page.getParagraphText()).toEqual('Welcome to app!');
+//   });
+// });
 describe('letslearn App', () => {
   let page: AppPage;
 
@@ -7,8 +19,44 @@ describe('letslearn App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('Should display Letslearn title', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getTitle()).toEqual('Letslearn');
   });
+
+  it('Should start with 1 point', () => {
+    page.navigateTo();
+    expect(page.getPoints()).toEqual('1');
+  });
+
+  it('Should increase points by clicking plus1', () => {
+    page.navigateTo();
+
+    expect(page.getPoints()).toEqual('1');
+    page.getPlus1Button().click();
+
+    expect(page.getPoints()).toEqual('2');
+
+    page.getPlus1Button().click();
+    page.getPlus1Button().click();
+    page.getPlus1Button().click();
+
+    expect(page.getPoints()).toEqual('5');
+  });
+
+  it('Should rest points by clicking plus1', () => {
+    page.navigateTo();
+
+    page.getPlus1Button().click();
+    page.getPlus1Button().click();
+    page.getPlus1Button().click();
+
+    expect(page.getPoints()).toEqual('4');
+
+    page.getResetButton().click();
+
+    expect(page.getPoints()).toEqual('0');
+  });
+
 });
+
